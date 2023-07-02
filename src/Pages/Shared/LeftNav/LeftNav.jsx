fd8d270c09adb03b553c604044b68abfebfb7979
Swadeb-Pzero <swadebsharmaphero88@gmail.com>
2023-07-02 22:15:08 +0600

    added left side navigation bar

--- src/Pages/Shared/LeftNav/LeftNav.jsx
+++ src/Pages/Shared/LeftNav/LeftNav.jsx
@@ -1,20 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(()=>{
-        fetch('http://localhost:5000/categories')
-        .then(res => res.json())
-        .then(data =>console.log(data))
-        .catch(error => console.log(error.message))
-    },[])
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.log(error.message));
+  }, []);
 
   return (
     <div>
-      <h2>Left nav</h2>
+      <h4>All Category</h4>
+      <div className="ps-4">
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link to={`/category/${category.id}`} className=" text-black text-decoration-none">{category.name}</Link>
+          </p>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LeftNav
+export default LeftNav;
